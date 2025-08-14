efc88d90bcb93b06cba2a856ed6423cce2019fe3
Harmandeep2312 <harman145singh@gmail>
2025-08-14 22:36:32 +0530

    fixed errors

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,8 +1,11 @@
 module.exports = function (fn) {
     if (!fn) {
-        console.error("wrapAsync received undefined function!");
+        console.error("⚠ ERROR: wrapAsync received an undefined function!");
     }
     return function (req, res, next) {
+        if (!fn) {
+            return next(new Error("wrapAsync got undefined function"));
+        }
         fn(req, res, next).catch(next);
     };
 };
